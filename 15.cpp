c492703968a088f3d32d306eed382d2ae6e681b1
wy_s5y2 <wy_s5y2@163.com>
2026-08-08 23:09:04 +0800

    06-8-8-1

---
+++ 15.cpp
@@ -0,0 +1,74 @@
+/*
+    用单链表保存m个整数，结点的结构为，且∣data∣≤n (n为正整数)
+    现要求设计一个时间复杂度尽可能高效的算法，
+    对于链表中 data 的绝对值相等的结点，仅保留第一次出现的结点而删除其余绝对值相等的结点。
+*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+#define MAXN 1001
+int d[MAXN];
+
+typedef struct Node{
+    int data;
+    struct Node *next; 
+} Node;
+
+Node *createlink(int a[], int m)
+{
+    Node *head = (Node *)malloc(sizeof(Node));
+    head->next = NULL;
+    Node *tail = head;
+
+    for(int i=0;i<m;i++){
+        Node *p = (Node *)malloc(sizeof(Node));
+        p->data = a[i];
+
+        tail->next = p;
+        p->next = NULL;
+        tail = p;
+    }
+    return head;
+}
+
+// 关键函数
+void deletenode(Node *h)
+{
+    for(int i=0;i<MAXN;i++){
+        d[i] = 0;
+    }
+    while(h->next != NULL){
+        if(d[abs(h->next->data)] == 1){
+            h->next = h->next->next;
+        }
+        else{
+            d[abs(h->next->data)] = 1;
+            h = h->next; 
+        }
+    }
+}
+
+void display(Node *h)
+{
+    h = h->next;
+    while(h != NULL){
+        cout<<h->data;
+        if(h->next != NULL) cout<<"->";
+        h = h->next;
+    }
+    cout<<endl;
+    return;
+}
+
+int main()
+{
+    int a[] = {21,-15,-15,-7,15};
+    int m = sizeof(a)/sizeof(a[0]);
+    Node *h = createlink(a,m);
+    
+    deletenode(h);
+    display(h);
+    return 0;
+}
